4a73f3512d20eab6bcdd349ef8e2d40082956c26
red_minter <oleg.a.fedyshyn@gmail.com>
2024-03-20 14:14:32 +0200

    added the website structure

---
+++ home.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-md-6">
+        <img src="images/home-image.jpg" alt="Home Image" class="img-fluid">
+    </div>
+    <div class="col-md-6">
+        <h2>Welcome to My Business Card Website</h2>
+        <p>This website showcases my personal/business information and projects. Explore the navigation above to learn more about me and get in touch.</p>
+    </div>
+</div>
